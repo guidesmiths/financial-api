1d819b834c46ec84f7fdfdcc452ddb087b717e9a
feliun <felipe.polo@gmail.com>
2018-01-25 18:35:08 +0100

    User verification using controller

--- controller.js
+++ controller.js
@@ -2,7 +2,16 @@ module.exports = (mongo, redis) => {
 
     const store = mongo.collection('data');
 
-    const getData = () => store.findOne({ episode_id: 4 });
+    const getData = ({ favEpisode }) => store.findOne({ episode_id: favEpisode });
 
-    return { getData };
+    const verifyUser = (user, pwd) => {
+        if (user !== 'user' || pwd !== 'password') throw new Error('User not found');
+        return Promise.resolve({
+            user,
+            favEpisode: 4,
+            random: Math.random() * 1000
+        });
+    };
+
+    return { getData, verifyUser };
 };
--- initApp.js
+++ initApp.js
@@ -2,24 +2,23 @@ const bodyparser = require('body-parser');
 const jwtExpress = require('express-jwt');
 const jwt = require('jsonwebtoken');
 
-
 module.exports = (app, controller, config) => {
 
     const initUnprotected = () => {
-        app.post('/authenticate', (req, res) => {
-            if(req.body.user !== 'user' || req.body.password !== 'password') return res.status(401).send({message: '401 bad email'});
-            return res.json({
-                success: true,
-                message: 'Enjoy your token!',
-                token: jwt.sign({ user: 'user' }, config.app.secretKey , { expiresIn: 60 * 60 })
-            });
-        });
+        app.post('/authenticate', (req, res) =>
+            controller.verifyUser(req.body.user, req.body.password)
+                .then((userData) => res.json({
+                        message: 'Enjoy your token!',
+                        token: jwt.sign(userData, config.app.secretKey , { expiresIn: 60 * 60 })
+                    }))
+                .catch((error) => res.status(401).send({ error }))
+        );
     };
 
     const initProtected = () => {
         app.use(jwtExpress({ secret: config.app.secretKey }));
         app.get('/test', (req, res) => {
-            controller.getData()
+            controller.getData(req.user)
                 .then((data) => res.json(data))
         });
     };
