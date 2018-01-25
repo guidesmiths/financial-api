4702210b46b9fa1a8c1822bfbbd13c4f6222c69d
feliun <felipe.polo@gmail.com>
2018-01-25 18:15:02 +0100

    Protecting test endpoint

--- initApp.js
+++ initApp.js
@@ -6,21 +6,19 @@ module.exports = (app, controller, config) => {
     app.use(bodyparser.urlencoded({ extended: true }));
     app.use(bodyparser.json());
 
-    app.get('/protected', jwtExpress({ secret: config.app.secretKey }),  (req,res) => {
-        res.sendStatus(200);
-    })
-
-    app.get('/test', (req, res) =>
-        controller.getData()
-            .then((data) => res.json(data))
-    );
-
     app.post('/authenticate', (req, res) => {
         if(req.body.user !== 'user' || req.body.password !== 'password') return res.status(401).send({message: '401 bad email'});
         return res.json({
             success: true,
             message: 'Enjoy your token!',
-            token: jwt.sign({ user: 'user'}, config.app.secretKey , { expiresIn: 60 * 60 })
+            token: jwt.sign({ user: 'user' }, config.app.secretKey , { expiresIn: 60 * 60 })
         });
+    });
+
+    app.use(jwtExpress({ secret: config.app.secretKey }));
+
+    app.get('/test', (req, res) => {
+        controller.getData()
+            .then((data) => res.json(data))
     });
 };
