d0ea886a3636720fd85b55ac6bac13166e2cc38b
feliun <felipe.polo@gmail.com>
2018-01-25 18:17:41 +0100

    Setting up protected/unprotected endpoints

--- initApp.js
+++ initApp.js
@@ -2,23 +2,30 @@ const bodyparser = require('body-parser');
 const jwtExpress = require('express-jwt');
 const jwt = require('jsonwebtoken');
 
+
 module.exports = (app, controller, config) => {
+
+    const initUnprotected = () => {
+        app.post('/authenticate', (req, res) => {
+            if(req.body.user !== 'user' || req.body.password !== 'password') return res.status(401).send({message: '401 bad email'});
+            return res.json({
+                success: true,
+                message: 'Enjoy your token!',
+                token: jwt.sign({ user: 'user' }, config.app.secretKey , { expiresIn: 60 * 60 })
+            });
+        });
+    };
+
+    const initProtected = () => {
+        app.use(jwtExpress({ secret: config.app.secretKey }));
+        app.get('/test', (req, res) => {
+            controller.getData()
+                .then((data) => res.json(data))
+        });
+    };
+
     app.use(bodyparser.urlencoded({ extended: true }));
     app.use(bodyparser.json());
-
-    app.post('/authenticate', (req, res) => {
-        if(req.body.user !== 'user' || req.body.password !== 'password') return res.status(401).send({message: '401 bad email'});
-        return res.json({
-            success: true,
-            message: 'Enjoy your token!',
-            token: jwt.sign({ user: 'user' }, config.app.secretKey , { expiresIn: 60 * 60 })
-        });
-    });
-
-    app.use(jwtExpress({ secret: config.app.secretKey }));
-
-    app.get('/test', (req, res) => {
-        controller.getData()
-            .then((data) => res.json(data))
-    });
+    initUnprotected();
+    initProtected();
 };
